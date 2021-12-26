e61101d273d8b0ce394fee4882d2b0852af6a56b
fakhriLeadbook <syalala_47@yahoo.com>
2021-12-26 19:41:47 +0700

    awalan

---
+++ src/app.py
@@ -0,0 +1,44 @@
+from flask import Flask, jsonify, request
+import pandas as pd
+import pymongo
+import boto3
+
+from env import env_var
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return "Leadbook Test"
+
+
+@app.route('/insert', methods=['POST'])
+def inst():
+
+    env_ = env_var()
+
+    s3 = boto3.resource('s3',
+                        aws_access_key_id=env_.aws_access_key_id,
+                        aws_secret_access_key=env_.aws_secret_access_key
+    )
+
+    obj = s3.meta.client.get_object(Bucket= request.json["bucket"], Key= request.json["file"]) 
+    df = pd.read_csv(obj['Body'], sep="\t")
+     
+
+    myclient = pymongo.MongoClient(env_.mongo_client)
+    mydb = myclient["leadbook_db"]
+    mycoll = mydb["tes1"]
+
+    to_dict = df.to_dict("records")
+    x = mycoll.insert_many(to_dict)
+
+    return jsonify({'result' : "success"})
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
+
+
