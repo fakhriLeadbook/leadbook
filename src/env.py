82e3a4fa95396662e9005ce9a76bb86632e01f59
fakhriLeadbook <syalala_47@yahoo.com>
2021-12-29 02:07:53 +0700

    74

--- src/env.py
+++ src/env.py
@@ -1,7 +1,7 @@
 import os
 
 class EnvironmentVar(): 
-    def access(self):
+    def __init__(self):
         self.aws_access_key_id = os.environ['aws_access_key_id'] 
         self.aws_secret_access_key = os.environ['aws_secret_access_key']
         self.mongo_client = os.environ['mongo_client']  
