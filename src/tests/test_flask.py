70cc482783e3306b01bf0b4d51be2bf0e4010f8a
fakhriLeadbook <syalala_47@yahoo.com>
2021-12-30 12:04:56 +0700

    93

--- src/tests/test_flask.py
+++ src/tests/test_flask.py
@@ -7,3 +7,23 @@ def test_index(app, client):
     assert expected == res.get_data(as_text=True)
 
 
+
+# def test_upload(app, client):
+    
+#     mimetype = 'application/json'
+#     headers = {
+#         'Content-Type': mimetype,
+#         'Accept': mimetype
+#     }
+
+    
+#     data = {"bucket": "senz-testing", "file": "Leadbook/data-2018.txt"}
+#     url = '/insert/'
+
+#     response = client.post(url, data=json.dumps(data), headers=headers)
+
+#     assert response.content_type == mimetype
+#     assert res.status_code == 200
+
+
+    
