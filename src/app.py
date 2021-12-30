2f3668752d22958fc9bc7da29548ca2c5cb1c5d6
fakhriLeadbook <syalala_47@yahoo.com>
2021-12-30 11:27:33 +0700

    90

--- src/app.py
+++ src/app.py
@@ -12,8 +12,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Leadbook Test 2"
-    # return jsonify({'result' : "success"})
+    # return "Leadbook Test 2"
+    return jsonify({'result' : "success"})
 
 
 @app.route('/insert', methods=['POST'])
--- src/tests/test_pytest.py
+++ src/tests/test_pytest.py
@@ -4,7 +4,9 @@ def test_index(app, client):
     res = client.get('/')
     assert res.status_code == 200
     expected = {'result' : "success"}
-    # assert expected == json.loads(res.get_data(as_text=True))
-    assert expected == res.get_data(as_text=True)
+    
+    print(json.loads(res.get_data(as_text=True)))
+    assert expected == json.loads(res.get_data(as_text=True))
+    # assert expected == res.get_data(as_text=True)
 
 
